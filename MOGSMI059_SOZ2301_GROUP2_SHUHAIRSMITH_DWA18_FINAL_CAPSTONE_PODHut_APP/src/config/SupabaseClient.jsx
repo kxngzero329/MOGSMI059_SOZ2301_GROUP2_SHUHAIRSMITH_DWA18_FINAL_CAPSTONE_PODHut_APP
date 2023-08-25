18a26670f57685db9e2e5131b584365cc03cdc7b
SMITH MOGAMAT SHUHAIR <126153069+kxngzero329@users.noreply.github.com>
2023-08-25 13:50:05 +0200

    changes was made by the genre function and preview tab

--- MOGSMI059_SOZ2301_GROUP2_SHUHAIRSMITH_DWA18_FINAL_CAPSTONE_PODHut_APP/src/config/SupabaseClient.jsx
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vlslwrnugfrtqoryqjdg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsc2x3cm51Z2ZydHFvcnlxamRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTExNDAyODIsImV4cCI6MjAwNjcxNjI4Mn0.lmi-oRQPrKmsGS4LyJKKoqr_2ez6cB6p4rifyoTs0Ws'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
